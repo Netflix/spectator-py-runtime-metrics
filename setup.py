661ff814917cc609eea3b28c3ee4e3b2a245d035
Tim Hatch <tim@timhatch.com>
2024-07-19 12:25:16 -0700

    Remove py2 from wheel, switch to static metadata (#8)
    * Omit universal option, we don't need a py2.py3 wheel
    
    * Switch to static metadata

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
-import os
-
 from setuptools import setup
 
-
-def read(fname):
-    """Utility function to read a file, for publishing the README with the package."""
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-setup(
-    name="netflix-spectator-py-runtime-metrics",
-    version="1.0.0rc0",
-    python_requires=">3.9",
-    description="Library to collect runtime metrics for Python applications using spectator-py.",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    author="Netflix Telemetry Engineering",
-    author_email="netflix-atlas@googlegroups.com",
-    license="Apache 2.0",
-    url="https://github.com/Netflix/spectator-py-runtime-metrics",
-    packages=["runmetrics"],
-    install_requires=["netflix-spectator-py==1.0.0rc1"],
-    extras_require={
-        "dev": [
-            "check-manifest",
-            "pylint",
-            "pytest-cov",
-            "pytest"
-        ]
-    }
-)
+setup()
